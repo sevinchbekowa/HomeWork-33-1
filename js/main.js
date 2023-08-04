bb8da4482d69b1a1ea288037f092dfd9fc3dd7b2
= <jonibekabdiroziqov@gmail.com>
2023-08-04 17:37:15 +0800

    changed all files

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// home-1:
+
+//  let number1 = +prompt('sonni kiriting:...')
+//  let number2 = +prompt('sonni kiriting:...')
+
+//   console.log(number1 - number2)
+//   console.log(number1 * number2)
+//   console.log(number1 / number2)
+//   console.log(number1 + number2)
+
+
+
+
+
+
+
+
+// home-2:
+
+let firstname =prompt('ismni kirit:...')
+let lastname = prompt('familyani kirit:...')
+let yosh = prompt('yoshingni kirit;')
+
+
+let student = {
+    name: firstname,
+    surname: lastname,
+    age: yosh,
+}
+
+console.log("ism: " + student['name'])
+console.log("familya: " + student['surname'])
+console.log("yosh: " + student['age'])
+
+
+
+
+
+
